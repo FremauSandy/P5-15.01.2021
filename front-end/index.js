3321e9a7d0525327ec1c782374d90655b84ed2f0
Fremau Sandy <loly-skull@hotmail.fr>
2021-01-26 13:25:55 +0100

    ajout tableau id products

--- front-end/index.js
+++ front-end/index.js
@@ -15,75 +15,31 @@ fetch(URL, {method: "GET"})
         for (let i = 0; i < cart.length; i++) {            /*préciser l'action "ajout"*/   
             cart[i].addEventListener('click', () => {      /*déclencheur click*/ 
             console.log('added to cart');                  /*appliquer le résultat dans console*/
-            cartNumbers(products[i]);            
+            cartNumbers(cart[i].dataset.product);            
         })
 }
       })
     .catch(err => console.log("Houston we have a problem", err)); /*si erreur !*/
 
-
-/*CIBLER LES CONTENEURS*/
+/*CIBLER LES CONTENEURS PAGE INDEX*/
 function appearCamera(camera){                                     
     
     let Article = document.createElement("article")                 // créer une envloppe pour un produit
-    document.getElementById("vintage-camera").appendChild(Article)  // cibler la div parent de l'article
+    document.getElementById("product-choice").appendChild(Article)    // cibler la div parent de l'article
     Article.classList.add("product-list")
 
     let imgProduct = document.createElement("img")                  // inclure emplacement image produit
     imgProduct.classList.add("img-article","small")                 // ajout d'une classe 
     imgProduct.src = camera.imageUrl
-    
-    let content = document.createElement("span")                    // content name et price
 
     let nameProduct = document.createElement("h3")                  // indiquer le "nom" du produit
     nameProduct.textContent = camera.name
 
-    let priceProduct = document.createElement("p")                  // element prix
-    priceProduct.classList.add("price")
-    priceProduct.textContent = camera.price/100 + "€"               // prix d'une camera
-
     let description = document.createElement("p")                   // description Lorem ...
     description.textContent = camera.description
 
-    let Lens = document.createElement("input")                     // élément choix de lentilles
-    Lens.classList.add("select-lens")
-
-    let addCart = document.createElement("a")                       // lien ajouter au panier
-    addCart.classList.add("add-cart")
-    addCart.href = "#" + camera._id
-    addCart.textContent = "Ajouter"
-    
-
     Article.appendChild(imgProduct)
-    Article.appendChild(content)
     content.appendChild(nameProduct)
-    content.appendChild(priceProduct)
     Article.appendChild(description)
-    Article.appendChild(Lens)
-    Article.appendChild(addCart)
-}
-
-/*cibler products*/ 
-
-
-    //??? recuperer l'id ???//
-    //??? recreer le tableau ???//
-    //??? appliquer un constructor ???//
-
-
-/*indiquer le nombre de produits ajouter au panier*/ 
-function cartNumbers(products) {
-    let productNumbers = localStorage.getItem('cartNumbers');      /*indiquer au localStorage l'ajout d'un produit*/
-    productNumbers  = parseInt(productNumbers);                    /*resultat du premier ajout*/
-
-    if( productNumbers ) {                                           /*indiquer au localStorage l'ajout d'autres produits*/
-        localStorage.setItem('cartNumbers', productNumbers + 1);
-        document.querySelector('.store span').textContent = productNumbers + 1;      /* afficher un ajout sur l'icone panier*/
-        }                                                                           /*résultat du autre ajout*/ 
-        else{
-            localStorage.setItem('cartNumbers', 1);                                 /*OU résultat sans ajout*/ 
-            document.querySelector('.store span').textContent = 1;
-
-        }
-        setItems(products);
+    
 }
