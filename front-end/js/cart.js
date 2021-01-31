14c4d5e39b3cc6df2f2da33f2cae422f57397373
Fremau Sandy <loly-skull@hotmail.fr>
2021-01-31 16:14:14 +0100

    essai cart

--- front-end/js/cart.js
+++ front-end/js/cart.js
@@ -1,18 +1,45 @@
-/* ENREGISTRER URL */
-const URL = "http://localhost:3000/api/cameras/";
 
-/*RECUPERER LES CAMERAS DEPUIS L'URL*/
 
-fetch(URL, {method: "GET"})
-    .then(response => response.json())                                      //convertit le resultat au format json
-    .then(cameras => {console.log(cameras);                                 //convertit le resultat sur la console 
-        cameras.forEach(camera =>{
-            appearCamera(camera);
-        });
-      })
-    .catch(err => console.log("Houston we have a problem", err)); 
+// 1.recuperer le localstorage "panier" 
+// 2.recuperer les produits du panier avec methode Get utilisé dans product (id) boucle pour recuperer id
+// 3.afficher les produits du panier (for each)
+// 4. faire product.js
 
-    // 1.recuperer le localstorage"panier" 
-    // 2.recuperer les produitse du panier avec methode Get utilisé dans product (id) boucle pour recuperer id
-    // 3.afficher les produits du panier (for each)
-    // 4. faire product.js
+/*CIBLER LES PRODUITS SELECTIONNES DANS LOCALSTORAGE*/
+
+function manageCart(){
+
+  let panier = localStorage.getItem('panier');                    // accéder au localstorage
+      panier = JSON.parse(panier);                                // methode parse analyse une string et construit sa valeur ou son objet
+  let contentItem = document.querySelector('#cart-items');        // cibler le conteneur du panier dans page cart
+
+}
+
+/*PRESENTATION DES PRODUITS SELECTIONNES DANS LE PANIER*/ 
+function appearCart(camera){
+
+  let itemContent = document.createElement('div')                 // conteneur pour un produit
+    itemContent.classList.add("item-content")
+
+  let imgProduct = document.createElement("img")                  // image produit
+    imgProduct.src = camera.imageUrl
+
+  let content = document.createElement("span")                    // content name et price
+  
+  let nameProduct = document.createElement("h3")                  // nom du produit
+    nameProduct.textContent = camera.name
+  
+  let priceProduct = document.createElement("p")                  // prix d'un produit
+    priceProduct.textContent = camera.price/100 + "€"
+  
+    let lens = document.createElement("select")                   // choix de lentilles
+    lens.classList.add("select-lens")
+
+    itemContent.appendChild(imgProduct)
+    itemContent.appendChild(content)
+    itemContent.appendChild(nameProduct)
+    itemContent.appendChild(priceProduct)
+    itemContent.appendChild(lens)
+
+  document.getElementById("cart-items").appendChild(itemContent)  // cibler la div parent de l'article
+}
--- front-end/js/index.js
+++ front-end/js/index.js
@@ -3,51 +3,47 @@ const URL = "http://localhost:3000/api/cameras";
 
 /*RECUPERER LES CAMERAS DEPUIS L'URL*/
 
-fetch(URL, {method: "GET"})
-    .then(response => response.json())                              // convertit le resultat au format json*/
-    .then(cameras => {console.log(cameras);                         // convertit le resultat sur la console*/  
-        cameras.forEach(camera =>{
-            appearCamera(camera);
+fetch(URL, {method: "GET"})                                         // préciser l'url et la methode 
+    .then(response => response.json())                              // convertit le resultat au format json
+    .then(cameras => {console.log(cameras);                         // indique le resultat 'camera' sur la console 
+        cameras.forEach(camera =>{                                  // indiquer pour chaque produit recuperé une disposition
+            appearCamera(camera);                                   // préciser la fonction "présentation"
         });
       })
-    .catch(err => console.log("HOUSTON !! we have a problem", err));   // si erreur !*/
+    .catch(err => console.log("HOUSTON !! we have a problem", err));// si erreur !
 
 
-/*CIBLER LES CONTENEURS PAGE*/
+/*PRESENTATION DES PRODUITS*/
 
 function appearCamera(camera){                                     
     
-    let link = document.createElement("a")
-    link.href = "product.html?id=" + camera._id
+    let link = document.createElement("a")                          // integrer un lien page index vers page produit
+    link.href = "product.html?id=" + camera._id                     // lié l'id d'un produit 
     link.classList.add("link-product")
 
-    let article = document.createElement("article")                 // créer une envloppe pour un produit
-    
+    let article = document.createElement("article")                 // créer une enveloppe pour un produit
     article.classList.add("product-list")
 
     let imgProduct = document.createElement("img")                  // inclure emplacement image produit
-    imgProduct.classList.add("img-article","small")                 // ajout d'une classe 
-    imgProduct.src = camera.imageUrl
+    imgProduct.classList.add("img-article","small")                 
+    imgProduct.src = camera.imageUrl                                // image spécifique a son produit
     
     let content = document.createElement("span")                    // content name et price
     content.classList.add("product-content")
 
-    let nameProduct = document.createElement("h3")                  // indiquer le "nom" du produit
+    let nameProduct = document.createElement("h3")                  // nom du produit
     nameProduct.textContent = camera.name
-    nameProduct.classList.add("title-product")
+    nameProduct.classList.add("title-product")                      // nom spécifique a son produit
 
-    let priceProduct = document.createElement("p")                  // element prix
+    let priceProduct = document.createElement("p")                  // prix d'un produit
     priceProduct.classList.add("price")
-    priceProduct.textContent = camera.price/100 + "€"               // prix d'une camera
-
-    let description = document.createElement("p")                   // description Lorem ...
-    description.textContent = camera.description
+    priceProduct.textContent = camera.price/100 + "€"               // prix spécifique a son produit
     
-    article.appendChild(imgProduct)
+    article.appendChild(imgProduct)                                 // un article contient une image et un content(nom et prix)
     article.appendChild(content)
     content.appendChild(nameProduct)
     content.appendChild(priceProduct)
-    link.appendChild(article)
-    document.getElementById("vintage-camera").appendChild(link)   // cibler la div parent de l'article
+    link.appendChild(article)                                      // lien entourant un article
+    document.getElementById("vintage-camera").appendChild(link)    // cibler la div parent de l'article
 }
 
