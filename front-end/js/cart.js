8f7eb23dc0a369694d0aa4670526b21f97a8e6e8
Fremau Sandy <loly-skull@hotmail.fr>
2021-02-01 18:37:48 +0100

    correction ??

--- front-end/js/cart.js
+++ front-end/js/cart.js
@@ -1,45 +1,68 @@
-
-
 // 1.recuperer le localstorage "panier" 
 // 2.recuperer les produits du panier avec methode Get utilisé dans product (id) boucle pour recuperer id
 // 3.afficher les produits du panier (for each)
 // 4. faire product.js
 
+/* ENREGISTRER URL */
+const apiUrl = "http://localhost:3000/api/cameras/";
+const firstURL = "http://localhost:3000/api/cameras";
+
+//cart = button html "ajouter"
+//appearCart = présentation des produits dans page cart
+//panier = produits dans localstorage
+
+/*RECUPERER L'ID ET URL D'UN PRODUIT */
+const parsedUrl = new URL(window.location.href);                  //créer un objet url a partir d'un "string"
+const idProduct = parsedUrl.searchParams.get("id");               // indique un Id précis en fonction du produit selectionné
+
 /*CIBLER LES PRODUITS SELECTIONNES DANS LOCALSTORAGE*/
+let panier = JSON.parse(localStorage.getItem('panier'));          // accéder au localstorage
+console.log(panier)
 
-function manageCart(){
+fetch(firstURL, {method: "GET"})                                        
+    .then(response => response.json())                              
+    .then(cameras => {console.log(cameras);                         
+        cameras.forEach(camera =>{                                  
+            appearCart(camera);                                   
+        });
+      })
 
-  let panier = localStorage.getItem('panier');                    // accéder au localstorage
-      panier = JSON.parse(panier);                                // methode parse analyse une string et construit sa valeur ou son objet
-  let contentItem = document.querySelector('#cart-items');        // cibler le conteneur du panier dans page cart
-
+/*VERIFICATION DU CONTENU PANIER*/
+function manageCart() {
+  
+  if (localStorage.getItem("panier") === null || localStorage.getItem("panier") === "[]") {  // si le panier contient un produit
+    document.querySelector("#cart-infos").parentNode.hidden = true;                          // affiche le contenu
+  } else {
+    document.querySelector("#cart-infos").parentNode.hidden = false;                         // n'affiche pas le contenu
+  }
 }
 
 /*PRESENTATION DES PRODUITS SELECTIONNES DANS LE PANIER*/ 
 function appearCart(camera){
 
   let itemContent = document.createElement('div')                 // conteneur pour un produit
-    itemContent.classList.add("item-content")
+      itemContent.classList.add("item-content")
 
   let imgProduct = document.createElement("img")                  // image produit
-    imgProduct.src = camera.imageUrl
+      imgProduct.src = camera.imageUrl
 
   let content = document.createElement("span")                    // content name et price
   
   let nameProduct = document.createElement("h3")                  // nom du produit
-    nameProduct.textContent = camera.name
+      nameProduct.textContent = camera.name
   
   let priceProduct = document.createElement("p")                  // prix d'un produit
-    priceProduct.textContent = camera.price/100 + "€"
+      priceProduct.textContent = camera.price/100 + "€"
   
-    let lens = document.createElement("select")                   // choix de lentilles
-    lens.classList.add("select-lens")
+  let lens = document.createElement("select")                     // choix de lentilles
+      lens.classList.add("select-lens")
 
     itemContent.appendChild(imgProduct)
     itemContent.appendChild(content)
-    itemContent.appendChild(nameProduct)
-    itemContent.appendChild(priceProduct)
+      itemContent.appendChild(nameProduct)
+      itemContent.appendChild(priceProduct)
     itemContent.appendChild(lens)
 
   document.getElementById("cart-items").appendChild(itemContent)  // cibler la div parent de l'article
 }
+
--- front-end/js/product.js
+++ front-end/js/product.js
@@ -1,8 +1,13 @@
 /* ENREGISTRER URL */
-const apiUrl = "http://localhost:3000/api/cameras";
+const apiUrl = "http://localhost:3000/api/cameras/";
+
+//cart = button html "ajouter"
+//appearCamera = présentation de produit camera
+//addCart =  action ajout au panier
+//saveProductCart = sauvegarde des produis selectionnés dans le panier
+//nbrItems = nombre d'ajout pour le même produit dans le panier
 
 /*RECUPERER L'ID ET URL D'UN PRODUIT */
-
 const parsedUrl = new URL(window.location.href);                  //créer un objet url a partir d'un "string"
 const idProduct = parsedUrl.searchParams.get("id");               // indique un Id précis en fonction du produit selectionné
 
@@ -19,51 +24,60 @@ fetch(apiUrl + idProduct, {method: "GET"})                       // préciser et
       })
 
     .catch(err => console.log("HOUSTON !! we have a problem", err));// si erreur !
-
+    
 /*PRESENTATION PRODUIT SELECTIONNE*/
 function appearCamera(camera){                                     
 
     let article = document.createElement("article")                 
-    article.classList.add("product-list")
+        article.classList.add("product-list")
 
     let imgProduct = document.createElement("img")                  
-    imgProduct.classList.add("img-article","small")                 
-    imgProduct.src = camera.imageUrl
+        imgProduct.classList.add("img-article","small")                 
+        imgProduct.src = camera.imageUrl
     
     let content = document.createElement("span")                    
-    content.classList.add("product-content")
+        content.classList.add("product-content")
 
     let nameProduct = document.createElement("h3")                  
-    nameProduct.textContent = camera.name
-    nameProduct.classList.add("title-product")
+        nameProduct.textContent = camera.name
+        nameProduct.classList.add("title-product")
 
     let priceProduct = document.createElement("p")                  
-    priceProduct.classList.add("price")
-    priceProduct.textContent = camera.price/100 + "€"               
+        priceProduct.classList.add("price")
+        priceProduct.textContent = camera.price/100 + "€"               
 
     let description = document.createElement("p")                   
-    description.textContent = camera.description
-    description.classList.add("infos-article")
+        description.textContent = camera.description
+        description.classList.add("infos-article")
 
-    let addCartButton = document.createElement("button")            // ajout button pour le panier                
-    addCartButton.id ="add-cart"                                    // ajout d'un id au button
-    addCartButton.textContent = "Ajouter"
+/*QUANTITE D'UN PRODUIT*/
+const quantity = document.createElement("select")                          //création d'un input select
+
+    for (let i = 0; i < [1, 2, 3, 4, 5, 6 , 7, 8, 9, 10].length; i++) {    // afficher une liste
+        const nbrItems = document.createElement("option")//
+            nbrItems.textContent = i + 1;
+        quantity.appendChild(nbrItems);
+    }
+
+    let addCartButton = document.createElement("button")           // ajout button pour le panier                
+        addCartButton.id ="add-cart"                               // ajout d'un id au button
+        addCartButton.textContent = "Ajouter"
     
-    article.appendChild(imgProduct)                                 // un article contient une image, un content (nom et prix), une description et un button propre a un produit selectionné
-    article.appendChild(content)
-    content.appendChild(nameProduct)
-    content.appendChild(priceProduct)
-    article.appendChild(description)
-    article.appendChild(addCartButton)
-    document.getElementById("vintage-camera").appendChild(article) // cibler la div parent de l'article
+    article.appendChild(imgProduct)                                // <article>
+    article.appendChild(content)                                   // <div>
+        content.appendChild(nameProduct)                               // <h3>
+        content.appendChild(priceProduct)                              // <p>
+    article.appendChild(description)                               // <p>
+    article.appendChild(quantity)                                  //<input>
+    article.appendChild(addCartButton)                             // <button>
+    document.getElementById("vintage-camera").appendChild(article) // div parent de l'article
 }
-
 
 /*INDIQUER L'AJOUT AU PANIER ET LOCALSTORAGE*/ 
 function addCart() {
     let panier = localStorage.getItem('panier');                             //on recupere le local storage
 
-    if( panier) {                                                            //si le panier existe
+    if(panier) {                                                             //si le panier existe deja
         const localStorageTab = JSON.parse(panier);                          // transforme en tableau
         if(localStorageTab.find(id=> id != idProduct)){                      // si l'id n'existe pas dans le tableau du localstorage != veut dire non egal
             localStorageTab.push(idProduct)                                  // push dans tableau products[id]
@@ -76,11 +90,20 @@ function addCart() {
     }else{                                                                  // si le panier n'existe pas
         localStorage.setItem('panier', JSON.stringify([idProduct]));        // on crée le panier
         document.querySelector('.store span').textContent = 1;
-
     }
 }
 
 //1.creer un modele panier (id,quantité,lens) dossier models
-
-
 //2.adapter ajout panier (objets panier) [id, quantité,lens]=objet
+
+ /*ETIQUETTE D'UN PRODUIT*/
+ function etiquetteProduct() {
+    let itemCamera = {
+      _id: _id,
+      image: imageUrl,
+      name: name,
+      lenses: value,
+      quantité: quantité,
+      price: price
+    }
+}
